fe3d0bc73673213cfb8288791e4abd0053ec1a9b
icimathieu <mathieu.riviere.perso@gmail.com>
2025-12-28 21:58:48 +0100

    création github transcription paddleocr mémoire

---
+++ pdf2image.py
@@ -0,0 +1,77 @@
+#imports (ne va pas fonctionner puisque env .venv vide cf "pip list"): 
+import os
+import re
+import json
+import argparse
+import pandas as pd
+from tqdm import tqdm
+from pathlib import Path
+from pdf2image import convert_from_path
+#il faut aussi que j'installe poppler non ?
+
+
+#fonctions : 
+def all_files_in_repo(repo_path): 
+    from glob import glob
+    files_name = []
+    for file in glob(repo_path+"*"):
+        files_name.append(file)
+    return(files_name)
+
+def pdf_2_jpegs(files_list): 
+    import glob
+    for file in tqdm(files_list) :
+        path = file+"/*.pdf"
+        pdf = glob(path) 
+        print(pdf)
+        convert_from_path(pdf[0],
+            dpi=200,
+            output_folder=file, 
+            poppler_path="/opt/homebrew/bin",
+            fmt="jpeg",
+            output_file="page")
+
+def jpegs_2_jsonls (file):
+    import glob
+    from tqdm.auto import tqdm
+    paths_images = sorted(glob.glob(file+"/*.jpg"))
+
+#il faut que j'ai défini ocr
+    out_path = file + ".jsonl"
+    with open(out_path, "w") as f:
+        for idx, img_path in enumerate(tqdm(paths_images, total=len(paths_images)), start=1):
+            res = ocr.predict(img_path)
+            f.write(json.dumps({"page": idx, "image": os.path.basename(img_path), "ocr": res}, ensure_ascii=False) + "\n")
+            f.flush()
+
+def jsonls_2_json (file):#ouvrir mes différents jsonl et en faire un seul json
+    jsonls_path = file+"/*.jsonl"
+    jsonl_files = glob.glob(jsonls_path)
+    records = []
+    for path in jsonl_files:
+        with open(path, "r") as f:
+            for line in f:
+                records.append(json.loads(line))
+
+    with open("merged_ocr.json", "w") as f:
+        json.dump(records, f, ensure_ascii=False, indent=2)
+
+    print(f"Fusionné {len(records)} entrées depuis {len(jsonl_files)} fichiers.")
+
+#supprimer les jpg dans mon répertoire
+
+
+
+#exécution : 
+
+if __name__ == "__main__":
+    print('oui')
+
+    #interagir avec argparse
+
+    #nettoyer mes pdfs en enlevant premières pages pdf ajoutées par la bnf
+
+
+
+
+
